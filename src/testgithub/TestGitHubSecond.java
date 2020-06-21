4d889536d0c39e742e0fdd9304880b7150386ec5
Pavel <USER@DESKTOP-IQNR3UP>
2020-06-22 00:10:08 +0300

    Testing working with GitHub (Add new method in TestGitHubSecond.java)

--- src/testgithub/TestGitHubSecond.java
+++ src/testgithub/TestGitHubSecond.java
@@ -4,8 +4,12 @@ package testgithub;
 public class TestGitHubSecond {
     private final int x = 0;
     
-    public void Test01(){
+    public void test01(){
         System.out.println("Test01 from TestGitHubSecond");
         System.out.println("X: " + x);
+    }
+    
+    public void newTest01(){
+        System.out.println("New method!");
     }
 }
