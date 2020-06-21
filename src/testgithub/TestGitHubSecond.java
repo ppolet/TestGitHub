9884d356fe1a5f7b30aca779f22e7aee87b069eb
Pavel <USER@DESKTOP-IQNR3UP>
2020-06-22 00:00:27 +0300

    Testing working with GitHub

--- src/testgithub/TestGitHubSecond.java
+++ src/testgithub/TestGitHubSecond.java
@@ -6,7 +6,10 @@
 package testgithub;
 
 public class TestGitHubSecond {
+    private final int x = 0;
+    
     public void Test01(){
         System.out.println("Test01 from TestGitHubSecond");
+        System.out.println("X: " + x);
     }
 }
