8e8ce297087faf3aaace8211eee8575544710252
USER <USER@DESKTOP-IQNR3UP>
2020-06-21 23:57:29 +0300

    Testing working with GitHub

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+public class TestGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Testing working with GitHub");
+    }
+    
+    public void test01(){
+        System.out.println("Test01");
+    }
+    
+    public void test02(){
+        System.out.println("Test02");
+    }
+    
+}
---
+++ src/testgithub/TestGitHubSecond.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+public class TestGitHubSecond {
+    public void Test01(){
+        System.out.println("Test01 from TestGitHubSecond");
+    }
+}
